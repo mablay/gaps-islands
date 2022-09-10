ab58204e3ce721e106b097e4a901fcc32493026e
Marc Ablay <marc.ablay@gmail.com>
2022-09-10 22:11:00 +0200

    changed module function signature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @typedef RangeType
- * @property {number} start begining of the range
- * @property {number} end end of the range
+ * @property {number|Date} start begining of the range
+ * @property {number|Date} end end of the range
  * @property {any} [value] optional data property
  */
 /** @typedef {Object.<string, any> & RangeType} Range */
@@ -10,11 +10,18 @@
  * Discovers gaps and islands in an array or ranges.
  * A range has a *start*, *end* and optionally a *value* property.
  * @param {Range[]} ranges array of ranges
- * @param {Function} [merge] handles value aggregation for overlapping ranges
+ * @param {Function} [reducer] reduces the value property of ranges that get merged into islands
  * @returns {Range[]}
  */
-export function islands (ranges, merge) {
-  const sorted = [...ranges].sort(startComperator)
+export function mergeIslands (ranges, reducer) {
+  const sorted = ranges
+    .map(({ start, end, value }) => {
+      if (value === undefined) {
+        return { start, end }
+      }
+      return { start, end, value }
+    })
+    .sort(startComperator)
   const results = []
   let island = sorted.shift()
   if (island === undefined) {
@@ -22,8 +29,8 @@ export function islands (ranges, merge) {
   }
   for (const range of sorted) {
     if (range.start <= island.end) {
-      if (typeof merge === 'function') {
-        island.value = merge(island, range)
+      if (typeof reducer === 'function') {
+        island.value = reducer(island, range)
       }
       if (island.end < range.end) {
         island.end = range.end
@@ -43,8 +50,8 @@ export function islands (ranges, merge) {
  * @returns {Range[]} the gaps between ranges.
  * Gaps don't have value properties
  */
-export function gaps (ranges) {
-  const isl = islands(ranges)
+export function findGaps (ranges) {
+  const isl = mergeIslands(ranges)
   if (isl.length < 2) return []
   const res = []
   for (let i = 1; i < isl.length; i++) {
